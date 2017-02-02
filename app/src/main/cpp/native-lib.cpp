9919eb20265e2e27a28fa9107bfb6747f0dce6d3
DongjaeLee <ehdwo112@naver.com>
2017-02-02 21:17:20 +0900

    git PRMIA

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,51 @@
 #include <jni.h>
-#include <string>
+#include <opencv2/opencv.hpp>
+#include <android/asset_manager_jni.h>
+#include <android/log.h>
 
-extern "C"
-jstring
-Java_kr_co_ipdisk_home35_prima_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+using namespace cv;
+using namespace std;
+
+extern "C" {
+
+JNIEXPORT void JNICALL
+Java_com_example_home_opencvtest2_MainActivity_loadImage(
+        JNIEnv *env,
+        jobject,
+        jstring imageFileName,
+        jlong addrImage) {
+
+    Mat &img_input = *(Mat *) addrImage;
+
+    const char *nativeFileNameString = env->GetStringUTFChars(imageFileName, JNI_FALSE);
+
+    string baseDir("/storage/emulated/0/");
+    baseDir.append(nativeFileNameString);
+    const char *pathDir = baseDir.c_str();
+
+    img_input = imread(pathDir, IMREAD_COLOR);
+
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_home_opencvtest2_MainActivity_imageprocessing(
+        JNIEnv *env,
+        jobject,
+        jlong addrInputImage,
+        jlong addrOutputImage) {
+
+    Mat &img_input = *(Mat *) addrInputImage;
+    Mat &img_output = *(Mat *) addrOutputImage;
+    Mat result_binary_image;
+    //cvtColor( img_input, img_input, CV_BGR2RGB);
+    //cvtColor( img_input, img_output, CV_RGB2GRAY);
+    cvtColor( img_input, img_input, CV_RGB2GRAY);
+    adaptiveThreshold(img_input, img_output,
+
+                      255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 10);
+   // threshold( img_input, img_output, 1, 255, THRESH_BINARY );
+    //blur( img_output, img_output, Size(5,5) );
+    //Canny( img_output, img_output, 50, 150, 5 );
+}
+
 }
