35ac5daea73cdb4d4dd77604ec6c9cc94e57e4bc
WonHo Bark <wonho.park83@gmail.com>
2017-02-09 20:55:36 +0900

    imwrite 함수 작성

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -150,7 +150,11 @@ Java_kr_co_ipdisk_home35_PRIMA_MainActivity_imageprocessing(
     interest = img_input(Rect(boundRect2[select].tl().x - 20, boundRect2[select].tl().y - 20,
                            plate_width + 40, plate_width * 0.3));
 
-    //imwrite("interest.jpg", interest, );  //TODO vector에 뭘넣어야되냐.... orz
+
+    vector<int> compression_params;
+    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+    compression_params.push_back(9);
+    imwrite("interest.jpg", interest, compression_params);
 
 }
 }
